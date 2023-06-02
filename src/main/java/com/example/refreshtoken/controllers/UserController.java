fec88e933ece1a17ace584b4fddd52bdd2fc89aa
gabriels5g <gabrielspxls@gmail.com>
2023-06-02 01:32:41 -0300

    finished crud

--- src/main/java/com/example/refreshtoken/controllers/UserController.java
+++ src/main/java/com/example/refreshtoken/controllers/UserController.java
@@ -5,11 +5,17 @@ import com.example.refreshtoken.models.UserModel;
 import com.example.refreshtoken.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -36,7 +42,45 @@ public class UserController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<UserModel>> getAllUsers() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "id")UUID id) {
+        Optional<UserModel> userModelOptional = userService.findById(id);
+
+            if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id")UUID id) {
+        Optional<UserModel> userModelOptional = userService.findById(id);
+
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        userService.delete(userModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body("User Spot deleted Successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id")UUID id, @RequestBody @Valid UserDTO userDTO) {
+        Optional<UserModel> userModelOptional = userService.findById(id);
+
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDTO, userModel);
+        userModel.setId(userModelOptional.get().getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.save(userModel));
     }
 }
--- src/main/java/com/example/refreshtoken/services/UserService.java
+++ src/main/java/com/example/refreshtoken/services/UserService.java
@@ -3,11 +3,15 @@ package com.example.refreshtoken.services;
 import com.example.refreshtoken.models.UserModel;
 import com.example.refreshtoken.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -31,8 +35,16 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
-    public List<UserModel> findAll() {
-        return userRepository.findAll();
+    public Page<UserModel> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
+    public Optional<UserModel> findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
 }
